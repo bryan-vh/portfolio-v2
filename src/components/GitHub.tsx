6f916c327f5ba514239b89d8c29062834cdb36b0
Bryan Van Horn <bryan.a.vanhorn@gmail.com>
2023-12-24 18:35:56 -0800

    upload first blog post and make changes as needed

--- src/components/GitHub.tsx
+++ src/components/GitHub.tsx
@@ -9,23 +9,21 @@ interface GitHubProps {
 
 export function GitHub({ repo }: GitHubProps) {
     return (
-        <Card className="shadow-none">
-            <CardHeader>
-                <CardTitle>{repo.name}</CardTitle>
-                <CardDescription>{repo.description}</CardDescription>
-            </CardHeader>
-            <CardContent>
+        <a href={repo.html_url}>
+            <div className="flex flex-col bg-muted rounded-xl p-4 gap-2">
+                <h2 className="text-lg hover:text-primary font-bold">{repo.name}</h2>
+                <p className="text-sm text-muted-foreground line-clamp-2">{repo.description}</p>
                 <div className="flex gap-4">
                     <div className="flex items-center">
                         {/* <Circle className="mr-1 w-3 h-3 fill-orange-400 text-orange-400"/> */}
-                        {repo.language}
+                        <p className="text-sm font-medium">{repo.language}</p>
                     </div>
                     <div className="flex items-center">
-                        <Star className="mr-1 w-4 h-4"/>
-                        {repo.stargazers_count}
+                        <Star className="mr-1 w-3 h-3"/>
+                        <p className="text-sm font-medium">{repo.stargazers_count}</p>
                     </div>
                 </div>
-            </CardContent>
-        </Card>
+            </div>
+        </a>
     )
 }
