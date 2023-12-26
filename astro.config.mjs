66f35abf3ba392df768eb2faa53ed2773f224c8e
Bryan Van Horn <bryan.a.vanhorn@gmail.com>
2023-12-25 21:14:47 -0800

    add analytics and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,21 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://www.bryanvanhorn.com',
   integrations: [mdx(), sitemap(), tailwind({
     applyBaseStyles: false
-  }), react()]
+  }), react()],
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true
+    },
+    speedInsights: {
+      enabled: true
+    }
+  })
 });
